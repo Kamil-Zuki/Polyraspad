82d0562e480e5d8843bd3bc30680a6700f87106d
Zuko <karatov7@gmail.com>
2026-07-17 18:29:21 +0300

    Add pull-submodules.ps1 script

---
+++ pull-submodules.ps1
@@ -0,0 +1,19 @@
+# Pull all Git submodules and update them to the latest tracking branch
+
+Write-Host "Updating and initializing Git submodules recursively..." -foregroundColor Cyan
+git submodule update --init --recursive
+
+if ($LASTEXITCODE -ne 0) {
+    Write-Error "Failed to initialize/update git submodules."
+    exit $LASTEXITCODE
+}
+
+Write-Host "Fetching and updating submodules to their remote tracking branches..." -foregroundColor Cyan
+git submodule update --recursive --remote
+
+if ($LASTEXITCODE -ne 0) {
+    Write-Error "Failed to update submodules to remote branch version."
+    exit $LASTEXITCODE
+}
+
+Write-Host "All submodules successfully updated!" -foregroundColor Green
